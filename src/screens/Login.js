94fba8646f7a91ee2f937915d25b345cdde76a68
rubeshrubz <rubesh.subburayalu@colanonline.com>
2024-03-21 15:31:31 +0530

    "Revert Login screen"

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Login() {
   return (
     <View style={styles.container}>
-      <Text>Login</Text>
+      <Text>LoginScreen</Text>
     </View>
   )
 }
