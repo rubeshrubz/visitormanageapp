e9d1357f9574143b0dff1682568fd3770ada7e13
Madhu11398 <madhu311sekar@gmail.com>
2024-03-21 12:18:29 +0530

    Created Main screen

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/screens/Login';
-
+import MainScreen from './src/screens/MainScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,6 +13,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown:false}}>
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="MainScreen" component={MainScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
